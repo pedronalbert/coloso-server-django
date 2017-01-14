e83bb568cfced54e3ef97868867e7ecd0d736ebd
Alberto Pedron <pedron.albert@gmail.com>
2017-01-14 09:06:24 -0400

    Initial commit

---
+++ coloso/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+from summoner import views as summonerViews
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^riot-api/(?P<region>\w{1,4})/summoner/by-name/(?P<summonerName>[\w\s]+)$', summonerViews.findByName),
+    url(r'^riot-api/(?P<region>\w{1,4})/summoner/(?P<summonerId>\d+)$', summonerViews.findById),
+    url(r'^riot-api/(?P<region>\w{1,4})/summoner/(?P<summonerId>\d+)/runes$', summonerViews.getRunes),
+]
